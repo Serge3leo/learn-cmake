4160cca897339cc3c365b1b486e5da79c2a5992b
Serguei E. Leontiev <leo@sai.msu.ru>
2026-03-15 19:27:18 +0300

    Пересмотр обходов, решаем CMAKE_C_COMPILE_FEATURES

--- workarounds/TinyCC.cmake
+++ workarounds/TinyCC.cmake
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: BSD-2-Clause
 # SPDX-FileCopyrightText: 2026 Сергей Леонтьев (leo@sai.msu.ru)
 
-if (CMAKE_C_COMPILER_ID STREQUAL "TinyCC")
+if (CMAKE_C_COMPILER MATCHES "tcc")
     execute_process(COMMAND "${CMAKE_C_COMPILER}" --version
                     RESULT_VARIABLE _wTinyCC_res
                     OUTPUT_VARIABLE _wTinyCC_out)
@@ -16,12 +16,11 @@ if (CMAKE_C_COMPILER_ID STREQUAL "TinyCC")
                 " '${CMAKE_C_COMPILER}' --version")
         return ()
     endif ()
+    set(CMAKE_C_COMPILER_ID "TinyCC")
     set(CMAKE_C_COMPILER_VERSION "${CMAKE_MATCH_1}")
 
-    include(Compiler/CMakeCommonCompilerMacros)
-
-    set(CMAKE_C99_STANDARD_COMPILE_OPTION -std=c99)
-    set(CMAKE_C99_EXTENSION_COMPILE_OPTION -std=c99)
+    set(CMAKE_C99_STANDARD_COMPILE_OPTION "-std=c99")
+    set(CMAKE_C99_EXTENSION_COMPILE_OPTION "-std=c99")
     set(CMAKE_C99_STANDARD__HAS_FULL_SUPPORT ON)  # TODO
     if (CMAKE_C_COMPILER_VERSION VERSION_GREATER_EQUAL "0.9.27")
         set(CMAKE_C11_STANDARD_COMPILE_OPTION "-std=c11")
